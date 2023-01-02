a96a9a03115be7acdb0edd2f7e389850deac207c
marc-mosca <marc.mosca@icloud.com>
2023-01-02 18:27:45 +0100

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Marc Mosca on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
