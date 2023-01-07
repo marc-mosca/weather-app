9090bb614d29658e8a9985a436d0b040fe902f4e
marc-mosca <marc.mosca@icloud.com>
2023-01-07 19:02:23 +0100

    Change Spacer() by padding in VStack

--- weather-app/ContentView.swift
+++ weather-app/ContentView.swift
@@ -30,8 +30,7 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
                 }
-                
-                Spacer()
+                .padding(.bottom, 40)
                 
                 HStack(spacing: 20) {
                     WeatherDayView(dayOfWeek: "Mar", imageName: "cloud.fill", temperature: 11)
